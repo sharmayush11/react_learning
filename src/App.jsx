52317c42239c748731f6873e67a1800f3a430eaa
sharmayush11 <ayusharma1127@gmail.com>
2026-07-24 17:08:52 +0530

    stateLifting

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import AutoSaveForm from "./AutoSaveFrom"
+import { useState } from 'react'
 
+function Parent () {
+  const [text , setText] = useState("")
 
-function App() {
   return (
-    <AutoSaveForm/>
+    <div>
+      <InputBox onChange={setText}/>
+      <h3>Types : {text}</h3>
+    </div>
   )
 }
 
+function InputBox({ onChange }){
+  return <input type="text" onChange={(e) => onChange(e.target.value)}></input>
+}
+
+
+function App(){
+  return(
+    <>
+    <Parent/>
+  </>
+  )
+}
 export default App
---
+++ src/StateLifting.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function StateLifting() {
+  return (
+    <div>StateLifting</div>
+  )
+}
+
+export default StateLifting
