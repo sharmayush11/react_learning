f8e1ea282a0155b8d6c07bbcc78e5bce49272698
sharmayush11 <ayusharma1127@gmail.com>
2026-08-24 18:20:58 +0530

    createPortal

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-// import Chat from './Chat.jsx'
-// import Example2 from './Example2.jsx'
-
-import Users from "./Users.jsx"
-
+// import { createPortal } from "react-dom"
+import { useState } from "react"
+import Modal from "./Modal"
+// import PortalTest from "./PortalTest"
 
 function App() {
+  const [open, setOpen] = useState(false)
+
   return (
-    <>
-      <Users/>
-    </>
+    
+     <div style={{padding:"30px"}}>
+      <h1>App Component</h1>
+      {/* <PortalTest /> */}
+      {/* { createPortal(<h1>Portal</h1>, document.body)} */}
+
+      <button onClick={() => setOpen(true)}>Open Modal</button>
+
+      <Modal isOpen={open} onClose={() => setOpen(false)}>
+          <h2>Hello from Modal</h2>
+          <p>This is a simple modal without context.</p>
+      </Modal>
+     </div>
+    
   )
 }
 
